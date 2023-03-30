cc9a7ed38179962cde4695ad0b29c01b716b7cf9
Antonio Gennaro <antgennaro@hotmail.com>
2023-03-29 21:11:47 -0300

    segunda versión HTML+JS (con indicación)

---
+++ ALG_MATE_enJS.js
@@ -0,0 +1,28 @@
+document.querySelector("body").innerHTML = `
+<h2># MI ALGORITMO PARA PREPARAR MATE EN BOMBILLA (enJS)</h2>
+<p> Para no ser muy largo asumo contar con un recipiente térmico de 1 Litro, que tenga un sistema de calentamiento
+    eléctrico que tarde mínimo 5 minutos en llevar 1 litro de agua a 80°C.</p>
+<img height=300 width=300 src="https://d3ugyf2ht6aenh.cloudfront.net/stores/929/820/products/1_431-001e2745b6bf8298bd16650642063215-1024-1024.webp"
+    alt="Jarra Térmica y Mate">
+<h3># INICIO</h3>
+<ol>
+    <li>Tengo al menos 100 gramos de yerba? Si es sí, continúo en 2, sino ir a 15.</li>
+    <li>Tengo al menos 750ml de agua? Si es sí, continúo en 3, sino ir a 15.</li>
+    <li>Tengo un recipiente térmico calentador de agua? Si es sí, continúo en 4, sino ir a 15.</li>
+    <li>Tengo mate y bobilla? Sí es sí, continuo en 5, sino ir a 15.</li>
+    <li>Poner el agua en el recipiente calentador de agua.</li>
+    <li>Activar el calentador para que caliente el agua.</li>
+    <li>Poner una cucharada de yerba en el mate.</li>
+    <li>Llegó la yerba hasta las 3/4 partes del mate? si es si, continuar en 9, sino volver a 7.</li>
+    <li>Tapar el mate con una mano y ponerlo boca abajo.</li>
+    <li>Agitar el mate durante 10 segundos.</li>
+    <li>Ederezar el mate en la mesa y quitar la mano de la boca del mate.</li>
+    <li>Colocar la bombilla desde uno de los costados hasta el fondo y empujarla hacia el centro.</li>
+    <li>El agua del recipiente comienza a producir burbujas del tamano de 1 a 2 milímetros de diámetro? Si es sí ir
+        a 14, sino esperar 5 segundos y volver a 13.</li>
+    <li>Desactivar el calentador de agua. Ir a 16.</li>
+    <li>Fin con ERROR: Faltan ingredientes o elementos.</li>
+    <li>Fin con Éxito!! Comenzar a cebar y a disfrutar el mate!!!</li>
+</ol>
+<h3># FIN</h3>
+`
